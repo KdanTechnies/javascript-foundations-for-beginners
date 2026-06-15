d288a337fa8d7573c91f20282cbc6898a686425c
Kalu Onuka Daniel <danielfrosh22@gmail.com>
2026-06-15 11:46:04 +0100

    Fix: initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+/* let name = "John";
+console.log(name)
+
+/* Changing name variable */
+/* Let allows us to update the value later , which we updated on our console.log
+
+name = "Sarah";  */
+/*
+let school = "Akara College of Health";
+let city = "Lagos";
+let hobby = "Smoking"; */
+
+/*
+const age = 18;
+*/
+
+/*
+const MyName = "Miracle";
+const favoriteFood = "Rice";
+const country = "Nigeria"; */
+
+/*
+let score = 20;
+score = 10;
+*/
+
+/*
+const birthYear = 2005;
+birthYear = 2006;
+*/
+
+/*
+const name = "Miracle"; /* This is called a string ("") 
+const age = 16; /* This is called a Number (18) 
+const isStudent = "true" /* This is called a Boolean ( which means "ture" or "false") 
+let country; /* This is called an Undefined data ty[pe]s */
+
+const MyHead = "Isi igbidi";
+MyHead = "akpu";
+let MyHead = "isi igbidi";
+Myhead = "akpu";
+const MyAge = 20;
+const isMad = "false";
